0f8278c1b3a6d248fec17e176c5ba5990bc0ee16
luciac <luciaconchello@yahoo.com>
2015-03-16 03:39:01 -0400

    SO SLEEEEPY

---
+++ robot 2/draw.js
@@ -0,0 +1,125 @@
+speed(20)
+
+var something=function(x) {
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+}
+
+var flower=function(x) {
+  something(x)
+  something(x)
+  something(x)
+  }
+
+
+flower(20)
+
+
+
+
+for (var i=0; i < 4; i++) {
+    move(60,0);
+    flower(5);
+  }
+  
+  
+  
+  var something=function(x) {
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+}
+
+var flower=function(x) {
+  something(x)
+  something(x)
+  something(x)
+  }
+
+
+flower(20)
+
+
+
+
+for (var i=0; i < 4; i++) {
+    move(0,60);
+    flower(5);
+  }
+  
+  
+  
+speed(20)
+
+var something=function(x) {
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+}
+
+var flower=function(x) {
+  something(x)
+  something(x)
+  something(x)
+  }
+
+
+flower(20)
+
+
+
+
+for (var i=0; i < 4; i++) {
+    move(60,0);
+    flower(5);
+  }
+  
+  
+  
+  var something=function(x) {
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+  rotate(60)
+  forward(x)
+}
+
+var flower=function(x) {
+  something(x)
+  something(x)
+  something(x)
+  }
+
+
+flower(20)
+
+
+
+
+for (var i=0; i < 4; i++) {
+    move(0,60);
+    flower(5);
+  }
---
+++ robot 2/drawtwo.js
@@ -0,0 +1,14 @@
+
+speed(90)
+
+angle(10);
+for(var x = 0; x < 100; x++) {
+  moveTo(x * 10,20)
+  rotate(6)
+  forward(18)
+  rotate(30)
+  forward(80)
+  move(30,0)
+  forward(200)
+  move(0,25);
+}
