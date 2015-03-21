30863022720983a3b1d22a679693779fbd8f3614
luciac <luciaconchello@yahoo.com>
2015-03-21 16:54:21 -0400

    boooooooooooo im hungersss for youuuuurrr touch a long and lonly timeeee
    god sppeeeedd yourrr lovee tooo meeeeeeeeeee

--- MassPaint/draw.js
+++ MassPaint/draw.js
@@ -30,6 +30,168 @@ $("#button-pen").on("click",function() {
 });
 
 
+
+
+
+
+
+
+
+/*THEEEEESSSE AREEEEEEEEEEEEEEEEEEEEEE
+EEEEEEEEEEEEEEcoloursEEEEEEEEEEEEEEEEE
+EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE */
+
+$("#clrred").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("red");
+	lineWidth(width);
+});
+
+$("#clrorange").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("orange");
+	lineWidth(width);
+});
+
+$("#clryellow").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("yellow");
+	lineWidth(width);
+});
+
+$("#clrnavajowhite").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("navajowhite");
+	lineWidth(width);
+});
+
+$("#clrgold").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("gold");
+	lineWidth(width);
+});
+
+$("#clrgreen").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("green");
+	lineWidth(width);
+});
+
+$("#clrskyblue").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("skyblue");
+});
+
+$("#clraquamarine").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("aquamarine");
+});
+
+$("#clrsteelblue").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("steelblue");
+});
+
+$("#clrazure").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("azure");
+});
+
+$("#clrchocolate").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("chocolate");
+});
+
+$("#clrgrey").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("grey");
+});
+
+$("#clrlightgrey").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("lightgrey");
+});
+
+$("#clrblack").on("click",function() {
+	$(".color").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineColor("black");
+});
+
+
+
+
+
+/*TTTTHHHHHHIIISS ISSS THHEEEEEEEEEEEEEEEEE
+EEEEEEEEEEEEEEEwidthEEEEEEEEEEEEEEEEEEEEEEE
+EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE*/
+
+$("#xsmall").on("click",function() {
+	$(".brushsize").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineWidth(1);
+});
+
+$("#small").on("click",function() {
+	$(".brushsize").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineWidth(4);
+});
+
+$("#med").on("click",function() {
+	$(".brushsize").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineWidth(8);
+});
+
+$("#large").on("click",function() {
+	$(".brushsize").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineWidth(12);
+});
+
+$("#xlarge").on("click",function() {
+	$(".brushsize").removeClass("selected");
+	$(this).addClass("selected");
+	tool="pen";
+	lineWidth(16);
+});
+
+
+
+
+
 var drawing = false;
 var tool = "pen";
 
@@ -62,3 +224,136 @@ $(document).on("mouseup",function(e) {
 
 
 
+
+
+
+
+
+$("#button-triangle").on("click",function() {
+ $(".tool").removeClass("selected");
+ $(this).addClass("selected");
+ tool = "triangle";
+ lineWidth(width);
+ lineColor(color);
+});
+
+
+function triangle() {
+  angle(0);
+  forward(50);
+  rotate(120);
+  forward(50);
+  rotate(120);
+  forward(50);
+  rotate(120);
+}
+
+
+
+$("#button-line").on("click",function() {
+ $(".tool").removeClass("selected");
+ $(this).addClass("selected");
+ tool = "line";
+ lineWidth(width);
+ lineColor(color);
+});
+
+
+function line() {
+  angle(0);
+  forward(50);
+}
+
+
+
+/*
+
+$("#button-bubble").on("click",function() {
+	$(".tool").removeClass("selected");
+	$(this).addClass("selected");
+	tool="circle";	
+});
+
+
+
+
+function circle() {
+	angle(0);
+	forward(10);
+	rotate(10);
+	forward(10);
+	rotate(10);
+	forward(10);
+	rotate(10);
+	forward(10);
+	rotate(10);
+	forward(10);
+	rotate(10);
+	forward(10);
+	rotate(10);}
+*/
+
+
+/*DISSSSSSS ISSSS THEEEEEEEEEEEEEEEEEEE
+EEEEEEEEEEEE sun EEEEEEEEEEEEEEEEEEEEEE
+EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE*/
+/*
+$("#button-sun").on("click",function() {
+	$(".tool").removeClass("selected");
+	$(this).addClass("selected");
+	tool="sun";
+	lineColor("orange");
+	lineWidth(8);	
+});
+
+
+
+function sun() {
+	function rays(x) {
+	rotate(315);
+	forward(x);
+	rotate(120);
+	forward(x);
+	move(15,-20);
+	rotate(290);
+	forward(x);
+	rotate(120);
+	forward(x);
+	move(29,9);
+	rotate(299);
+	forward(x);
+	rotate(120);
+	forward(x);
+	move(8,30);
+	rotate(300);
+	forward(x);
+	rotate(120);
+	forward(x);
+	move(-18,20);
+	rotate(297);	
+	forward(x);
+	rotate(120);
+	forward(x);
+	move(-30,-8);
+	rotate(298);
+	forward(x);
+	rotate(120);
+	forward(x);
+	move(-40,13);
+	 }
+
+	var circle=function(x) {
+	forward(x);
+	rotate(x); };
+
+	circle(20);
+
+	speed(1000);
+	for (var i=0; i < 35; i++) {
+		circle(10); }
+
+	rays(30);
+	
+	}
+	
+*/
