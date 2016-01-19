69421dd7edacd1e64e2fc2129a3edc5c56c82750
luciac <luciaconchello@yahoo.com>
2016-01-19 03:19:22 -0500

    IM A PLATAPUS FROM PLUTO
    HOW THE FUCK DO YOU SPELL PLATAPUS PLATEPUS PLAETOPUS FUCK THE ENGLISH
    WHERE DID THE INTERNET GO!?!?!?! WHYYYYYYYYYYYYY

---
+++ evanscat/brotatochip.js
@@ -0,0 +1,38 @@
+
+$(".scrolling").scrollFlight();
+$("#trigger").scrollFlight();
+
+
+
+/*
+$(".scrolling").on("arrived",function(e) {
+	$("#dacat").addClass("pop-up");
+	});
+*/
+
+$("#trigger").on("arrived",function(e) {
+	$("#dacat").addClass("riseup");
+});
+
+
+
+$("#dacat").hover(
+	function() {
+		$("#birthday").addClass("fade-in");
+		$("#head").addClass("balooned");
+		$(".lefti").addClass("widened");
+		$(".righti").addClass("widened");
+		$(".glimmer").addClass("widened");
+		$("#nose").addClass("move");
+		$(".ear").addClass("dissapear")
+		$(".down").addClass("moveit");
+		$(".trileft").addClass("moveit");
+		$(".triright").addClass("moveit");
+		$("#loon").addClass("descend");
+		$("#dacat").addClass("flyaway");
+		$("#raining").addClass("makeitrain");
+		$("#raining2").addClass("makeitrain");
+		$("#cover").addClass("goaway");
+		$("#instruct").addClass("fade-out");
+	});
+	
