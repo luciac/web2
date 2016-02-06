0e281c5357e0bdc0020776ad3d2c51404c3e1bd8
luciac <luciaconchello@yahoo.com>
2016-02-05 23:13:46 -0500

    WHO ARE THE BISCUITS OF GRAVY TOWN!?
    your mothers left testicle from her mother's father's grandaunt's side.
    Thats who.

---
+++ zombies/brotatochip.js
@@ -0,0 +1,48 @@
+
+$("#trigger").scrollFlight();
+$(".TOCtoggle").scrollFlight();
+
+
+
+/*
+$(".scrolling").on("arrived",function(e) {
+	$("#dacat").addClass("pop-up");
+	});
+*/
+
+$("#trigger").on("arrived",function(e) {
+	$(".hand").addClass("riseup");
+	$(".welcometitle").addClass("riseup");
+	$(".right").addClass("fadein");
+});
+
+$(".TOCtoggle").on("click",function(e) {
+	$("#homepage").addClass("fadeout");
+	$("#TOC").addClass("fadein");
+	console.log("your mom sucks");
+});
+
+
+
+
+
+$("#dacat").hover(
+	function() {
+		$("#birthday").addClass("fade-in");
+		$("#head").addClass("balooned");
+		$(".lefti").addClass("widened");
+		$(".righti").addClass("widened");
+		$(".glimmer").addClass("widened");
+		$("#nose").addClass("move");
+		$(".ear").addClass("dissapear")
+		$(".down").addClass("moveit");
+		$(".trileft").addClass("moveit");
+		$(".triright").addClass("moveit");
+		$("#loon").addClass("descend");
+		$("#dacat").addClass("flyaway");
+		$("#raining").addClass("makeitrain");
+		$("#raining2").addClass("makeitrain");
+		$("#cover").addClass("goaway");
+		$("#instruct").addClass("fade-out");
+	});
+	
